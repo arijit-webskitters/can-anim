e47dcc1092fce587353d55877277e7434011ac17
arijit-webskitters <155645185+arijit-webskitters@users.noreply.github.com>
2024-01-16 13:21:58 +0530

    Update can.js

--- can.js
+++ can.js
@@ -22,8 +22,8 @@ let model;
       1000
     );
     camera.position.z = 5;
-    camera.position.y = 1.8;
-    camera.lookAt(new THREE.Vector3(0, 1.8, 0));
+    camera.position.y = 2.1;
+    camera.lookAt(new THREE.Vector3(0, 2.1, 0));
 
 
     // const controls = new THREE.OrbitControls(
@@ -33,30 +33,21 @@ let model;
     // controls.target.z = 100;
 
     // const ambLight = new THREE.AmbientLight("#d6d6d6", 5);
-    const light1 = new THREE.DirectionalLight("#fff", 0.8);
-    light1.position.set(-0.15, 1.5, 1).normalize();
-    // light1.position.set(30, 1, 1).normalize();
-    light1.rotation.set(0, 0, 4);
+    const light1 = new THREE.DirectionalLight("#fff", 0.2);
+    light1.position.set(-60, 15, 10).normalize();
+    // light1.rotation.set(0, 0, 0);
+    // light1.position.set(0, 5, 5).normalize();
     // light1.position.set(-0.15, 1.5, 1).normalize();
     light1.castShadow = true;
     scene.add(light1);
 
-    const light2 = new THREE.PointLight(0xFFFFFF, 0.2);
-    light2.position.set(-3000, -500, 800);
-    light2.castShadow = true;
+    const light2 = new THREE.PointLight(0xFFFFFF, 0.5);
+    light2.position.set(40, 2, 10);
+    // light2.castShadow = true;
     scene.add(light2);
 
-    // const helper = new THREE.HemisphereLightHelper(light, 5)
-    // scene.add(helper)
-    // // light controls
-    // const lightColor = {
-    //   color: light.color.getHex(),
-    //   groundColor: light.groundColor.getHex()
-    // }
-    // const lightFolder = gui.addFolder('Light')
-
     const light3 = new THREE.HemisphereLight(0xFFFFFF, 1);
-    light3.position.set(-0.5, -5, 5);
+    light3.position.set(-0.5, -5, 10);
     // light3.castShadow = true;
     scene.add(light3);
 
@@ -71,7 +62,7 @@ let model;
 
     new THREE.GLTFLoader().load(
       // new THREE.OBJLoader().load(
-      "https://arijit-webskitters.github.io/can-anim/model3/Can.gltf",
+      "https://arijit-webskitters.github.io/can-anim/model3/Untitled.gltf",
       // "model2/can.obj",
 
       // called when the resource is loaded
@@ -81,7 +72,7 @@ let model;
           if (node instanceof THREE.Mesh) {
             node.castShadow = true;
             node.material.side = THREE.DoubleSide;
-            node.material.flatShading = true;
+            node.material.flatShading = false;
             // node.material.blendDst = 1;
             // node.material.needsUpdate = true;
             // node.material.metalness = 1;
@@ -89,7 +80,7 @@ let model;
 
 
 
-            node.material.color.setHex(0x282526);
+            // node.material.color.setHex(0x282526);
             // node.material.color = "#000";
             // node.material.map = textures[0];
             model = gltf.scene;
@@ -102,7 +93,7 @@ let model;
         // model.scale.set(0.35, 0.35, 0.35);
         // model.scale.set(0.1, 0.1, 0.1);
         // model.scale.set(12, 12, 12);
-        model.scale.set(0.01225, 0.01225, 0.01225);
+        model.scale.set(1, 1, 1);
         // model.position.set(0,-1.2,0);
         // scene.add(model);
         objGroup.add(model);
@@ -131,7 +122,7 @@ let model;
 
 
     const texture = new THREE.TextureLoader().load('https://arijit-webskitters.github.io/can-anim/images/wrap-img.png');
-    const geometry = new THREE.CylinderGeometry(0.6256, 0.6256, 3.5, 30, 5, true);
+    const geometry = new THREE.CylinderGeometry(0.6765, 0.6765, 3.61, 30, 5, true);
     // const material = new THREE.MeshBasicMaterial({ map: texture });
     // material.side = THREE.DoubleSide;
     // const cylinder = new THREE.Mesh(geometry, material);
@@ -139,9 +130,9 @@ let model;
     var phongMaterial = new THREE.MeshPhongMaterial({
       map: texture,
       shininess: 800,
-      alphaMap: texture,
-      blending: 1,
-      transparent: true,
+      // alphaMap: texture,
+      // blending: 1,
+      // transparent: true,
     });
     phongMaterial.side = THREE.DoubleSide;
 
@@ -156,7 +147,7 @@ let model;
     // cylinder.rotation.set(0, (Math.PI / 1.1), 0);
     cylinder.rotation.set(0, (Math.PI / 1.1), 0);
     // cylinder.position.set(-0.265, 1.75, 0.2);
-    cylinder.position.set(0, 1.9, 0);
+    cylinder.position.set(0, 1.895, 0);
     // cylinder.position.set(-0.265,0.55,0.2);
     // scene.add(cylinder);
 
