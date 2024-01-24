e92f5b0dd38b95560dea3627b18bbced373b0587
arijit-webskitters <155645185+arijit-webskitters@users.noreply.github.com>
2024-01-24 19:01:08 +0530

    Update banner-can.js

--- banner-can.js
+++ banner-can.js
@@ -136,7 +136,7 @@ function cnvasMove(){
         -100,
         Math.min(100, canvas2.clientHeight / 2 - e.clientY)
       );
-    lFollowX = (2 * lMouseX) / 500;
+    lFollowX = (2 * lMouseX) / 5000;
     lFollowY = (2 * lMouseY) / 2000;
   });
   animate();
